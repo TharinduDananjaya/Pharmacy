bffab1b7d637c7cd6645974534fa8e302ffa467f
Tharindu Dhananjaya <herathhmtd.20@uom.lk>
2025-03-06 14:54:58 +0530

    Refactor image handling in store method to directly use array instead of JSON encoding

--- app/Http/Controllers/Client/Prescription/ClientPrescriptionController.php
+++ app/Http/Controllers/Client/Prescription/ClientPrescriptionController.php
@@ -31,38 +31,6 @@ class ClientPrescriptionController extends Controller
         return Inertia::render('Client/Prescription/Create/Index', []);
     }
 
-    // public function store(Request $request)
-    // {
-    //     // Validate the incoming request
-    //     $validated = $request->validate([
-    //         'images' => 'required|array|max:5',
-    //         'images.*' => 'image|mimes:jpg,jpeg,png,pdf|max:2048',
-    //         'note' => 'nullable|string|max:500',
-    //         'delivery_address' => 'required|string|max:255',
-    //         'delivery_time' => 'required|string',
-    //     ]);
-
-    //     // Handle the uploaded images
-    //     $imagePaths = [];
-    //     if ($request->has('images')) {
-    //         foreach ($request->file('images') as $image) {
-    //             // Store each image in the 'prescriptions' directory and get the path
-    //             $imagePaths[] = $image->store('prescriptions', 'public');
-    //         }
-    //     }
-
-    //     // Create a new Prescription in the database
-    //     $prescription = Prescription::create([
-    //         'user_id' => auth()->id(),  // assuming the user is authenticated
-    //         'note' => $validated['note'],
-    //         'delivery_address' => $validated['delivery_address'],
-    //         'delivery_time' => $validated['delivery_time'],
-    //         'images' => json_encode($imagePaths),  // Store image paths as JSON
-    //     ]);
-
-    //     // Optionally, you can return a success response or redirect
-    //     return redirect()->route('client.prescriptions.index')->with('success', 'Prescription uploaded successfully.');
-    // }
 
     public function store(StorePrescriptionRequest $request)
     {
@@ -75,7 +43,7 @@ class ClientPrescriptionController extends Controller
             'note' => $request->note,
             'delivery_address' => $request->delivery_address,
             'delivery_time' => $request->delivery_time,
-            'images' => json_encode($imagePaths),  // Store image paths as JSON
+            'images' => $imagePaths, // Store image paths as JSON
         ]);
 
         // Optionally, you can return a success response or redirect
